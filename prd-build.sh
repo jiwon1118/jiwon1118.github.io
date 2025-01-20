2c5ddb448686e082c3583bc8c52a3d4f7fd96584
jiwon1118 <b23386585@gmail.com>
2025-01-20 10:59:15 +0900

    Update prd-build.sh

--- prd-build.sh
+++ prd-build.sh
@@ -2,7 +2,7 @@
 pnpm docs:build
 cp CNAME docs/
 cp robots.txt docs/
-cp naver7b7f7dc6cce8eb9498e18ebf4ae99fc9.html docs/
+cp naver0cece529713db95c189b5959b592c94d.html docs/
 git add .
 git commit -m "build: update docs"
-
+git push
