aad33b945e5e1a173fdfe437eb7fc59546012f0b
jiwon1118 <b23386585@gmail.com>
2025-01-23 10:33:57 +0900

    코드 수정

--- cp-files.sh
+++ cp-files.sh
@@ -5,6 +5,6 @@ pwd
 cp CNAME docs/
 cp robots.txt docs/
 cp naver0cece529713db95c189b5959b592c94d.html docs/
-cp googleaedd4bfcf8a4dd20.html/
+cp googleaedd4bfcf8a4dd20.html docs/
 cat /etc/os-release
 
